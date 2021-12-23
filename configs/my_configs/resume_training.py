f3bbde5c5985d2fd42665ab3a89e7f79f3ceb602
giladcohen <giladadlig@gmail.com>
2021-12-23 06:25:25 +0200

    add aspp_head_ext

--- configs/my_configs/resume_training.py
+++ configs/my_configs/resume_training.py
@@ -14,7 +14,7 @@ log_config = dict(
 optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0005,
                  paramwise_cfg=dict(
                      custom_keys={
-                         'backbone': dict(lr_mult=0.0)}))
+                         'ResNetV1c': dict(lr_mult=0.0)}))
 optimizer_config = dict()
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
