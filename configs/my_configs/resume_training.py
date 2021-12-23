9dbfb4eaf72bbcb4d50c4164c379ca207f55a22c
giladcohen <giladadlig@gmail.com>
2021-12-23 06:28:30 +0200

    add aspp_head_ext

--- configs/my_configs/resume_training.py
+++ configs/my_configs/resume_training.py
@@ -27,6 +27,19 @@ find_unused_parameters = True
 
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 model = dict(
+    type='EncoderDecoder',
+    pretrained='open-mmlab://resnet50_v1c',
+    backbone=dict(
+        type='ResNetV1c',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        dilations=(1, 1, 2, 4),
+        strides=(1, 2, 1, 1),
+        norm_cfg=norm_cfg,
+        norm_eval=False,
+        style='pytorch',
+        contract_dilation=True),
     decode_head=dict(
         type='ASPPHeadExt',
         in_channels=2048,
