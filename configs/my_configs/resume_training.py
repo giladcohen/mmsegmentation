eccfb3cfba12be5f79c7792e0a9a592a97054616
giladcohen <giladadlig@gmail.com>
2021-12-23 03:41:09 +0200

    add resume trainin config

---
+++ configs/my_configs/resume_training.py
@@ -0,0 +1,10 @@
+_base_ = [
+    '../_base_/models/deeplabv3_r50-d8.py',
+    '../_base_/datasets/pascal_voc12_aug.py', '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_40k.py'
+]
+model = dict(
+    decode_head=dict(num_classes=21), auxiliary_head=dict(num_classes=21))
+runner = dict(type='IterBasedRunner', max_iters=4100)
+checkpoint_config = dict(by_epoch=False, interval=50)
+evaluation = dict(interval=50, metric='mIoU', pre_eval=True)
