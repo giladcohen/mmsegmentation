d4ecba1264ad1cea069583e9d358c6f67cb10777
giladcohen <giladadlig@gmail.com>
2022-01-06 04:26:45 +0200

    u

--- configs/my_configs/train_glove.py
+++ configs/my_configs/train_glove.py
@@ -17,7 +17,7 @@ optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005,
                          'backbone': dict(lr_mult=0.0, decay_mult=0.0)}))
 optimizer_config = dict()
 # learning policy
-lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
+lr_config = dict(policy='poly', power=0.9, min_lr=0.0, by_epoch=False)  # min_lr was 1e-4
 # runtime settings
 runner = dict(type='IterBasedRunner', max_iters=10000)
 checkpoint_config = dict(by_epoch=False, interval=1000)
