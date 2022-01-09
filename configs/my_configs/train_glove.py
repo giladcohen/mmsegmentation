2b242adb022c553be48f42e9e81546c94348466d
giladcohen <giladadlig@gmail.com>
2022-01-09 19:48:07 +0200

    u

--- configs/my_configs/train_glove.py
+++ configs/my_configs/train_glove.py
@@ -19,8 +19,8 @@ optimizer_config = dict()
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-7, by_epoch=False)  # min_lr was 1e-4
 # runtime settings
-runner = dict(type='IterBasedRunner', max_iters=80000)
-checkpoint_config = dict(by_epoch=False, interval=40000)
+runner = dict(type='IterBasedRunner', max_iters=40000)
+checkpoint_config = dict(by_epoch=False, interval=20000)
 evaluation = dict(interval=100, metric='mIoU', pre_eval=True)
 find_unused_parameters = True
 
