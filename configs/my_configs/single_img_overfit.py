f350879d03f0b7ed46e67411062167d23ea9d72d
giladcohen <giladadlig@gmail.com>
2022-01-13 11:27:56 +0200

    add single image memorization training config

--- configs/my_configs/single_img_overfit.py
+++ configs/my_configs/single_img_overfit.py
@@ -98,8 +98,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=1,
-    workers_per_gpu=1,
+    samples_per_gpu=2,
+    workers_per_gpu=2,
     train=dict(
         emb=dict(emb_selection='glove', emb_path='/data/gilad/logs/glove_emb/pascal/glove_idx_to_emb.npy'),
         type=dataset_type,
