89ae7dec5866f1020c4b5a238a24475b5bca2fd1
giladcohen <giladadlig@gmail.com>
2022-01-15 03:06:23 +0200

    remove debug prints

--- mmseg/models/losses/distance_loss.py
+++ mmseg/models/losses/distance_loss.py
@@ -76,10 +76,6 @@ class DistanceLoss(nn.Module):
             class_weight = self.class_weight.index_select(0, labels)
         else:
             class_weight = None
-        # print('DEBUG({}): labels.shape={}\nlabels.unique()={}\nlabels={}'
-        #       .format(self.dist_criterion.__class__, labels.shape, labels.unique(), labels))
-        # print('DEBUG({}): weights.shape={}\nweights.unique()={}\nweights={}'
-        #       .format(self.dist_criterion.__class__, class_weight.shape, class_weight.unique(), class_weight))
         loss = self.dist_criterion(embs, embs_gt, class_weight)
         return loss
 
